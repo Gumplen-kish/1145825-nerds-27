4c0b5f73a86ccfa5ad891e620daae2d06509cc25
Gumplen <55996112+Gumplen-kish@users.noreply.github.com>
2020-02-25 18:50:35 +0700

    Добавляем JS

---
+++ js/popap.js
@@ -0,0 +1,73 @@
+/*Обьявляем, необходимые для окна обратной связи, переменные*/ 
+var button = document.querySelector(".btn-contact");
+var popup = document.querySelector(".write-us");
+var close = popup.querySelector(".close");
+var form = popup.querySelector("form");
+var name = popup.querySelector("[name=user-name]");
+var email = popup.querySelector("[name=email]");
+var textarea = popup.querySelector("[name=text-letter]");
+var isStorageSupport = true;
+var storage = "";
+/*Переменные для слайдера*/
+var slider=document.querySelector("slider-list");
+var totalSlider=slider.children;
+var totalToggles=document.querySelectorAll(".slider-toggle");
+/*Слайдер */
+function totalSlider (toggles,slider) {
+    for (var i=0; i<toggles.length; i++) {
+        toggles[i]addEventListener("click"function() {
+            for (var j=0;j<slider.length;j++) {
+                slider[j].classList.remove("active");
+                slider[i].classList.add("active");
+                toggles[j]classList.remove("active-toggle");
+                toggles[i]classList.add("active-toggle");
+            }
+        });
+    }
+}
+try {
+    storage = localStorage.getItem("name");
+    storage = localStorage.getItem("email");
+} catch (err) {
+    isStorageSupport=false;
+}
+/*Задаем добавление класса при клике на кнопку*/
+button.addEventListener("click",function(evt) {
+    popup.classList.add("write-us-show");
+    if  (storage) {
+        user-name.value=storage;
+        email.focus();
+    } else {
+    name.focus();
+    }
+});
+/*Задаем удаление класса при клике на кнопку*/
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove(".write-us-show");
+    popup.classList.remove("write-us-show-error");
+});
+
+/*Отменяем отправку форму если.. */
+form.addEventListener("submit",function(evt) {
+    if (!user-name.value ||!email.value) {
+        evt.preventDefault();
+        popup.classList.remove("write-us-show-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("write-us-show-error");
+        console.log("Введите ваше имя и email");
+    } else {
+        if (isStorageSupport) { 
+            localStorage.setItem("name",user-name.value);
+        }
+    }
+});
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("write-us-show")) {
+          popup.classList.remove("write-us-show");
+          popup.classList.remove("write-us-show-error");
+        }
+    }
+});
